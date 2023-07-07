3d267cfe164b112f72784e8d8b6a6d832a7cdd18
Louise Naginski <louisindiana@gmail.com>
2023-07-07 10:59:11 -0400

    check nfc functionality

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -21,6 +21,34 @@ React.useEffect(() => {
 }, []);
 
 function renderNfcButtons() {
+  if (hasNfc === null) {
+    return nulll
+  } else if (!hasNfc) {
+    return (
+      <View style={styles.wrapper}>
+        <Text>Your device doesn't support NFC</Text>
+      </View>
+    );
+  } else if (!enabled) {
+    return (
+      <View style={styles.wrapper}>
+        <Text>Your NFC is not enabled</Text>
+        <TouchableOpacity
+        onPress={() => {
+          NfcManager.goToNfcSetting();
+        }}>
+          <Text>GO TO NFC SETTINGS</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        onPress={async () => {
+          setEnabled(await NfcManager.isEnabled());
+        }}>
+          <Text>CHECK AGAIN</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
   return (
     <View style={styles.bottom}>
         <Button mode="contained" style={[styles.btn]}
