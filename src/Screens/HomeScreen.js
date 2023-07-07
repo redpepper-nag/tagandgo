c276b6780079bb04a8a39ef4ee58324f77f0e540
Louise Naginski <louisindiana@gmail.com>
2023-07-07 11:28:03 -0400

    nfc reading capabilities

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,76 +1,90 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {Button} from 'react-native-paper';
-import NfcManager from 'react-native-nfc-manager';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-paper';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
 function HomeScreen(props) {
-  const {navigation} = props;
+  const { navigation } = props;
   const [hasNfc, setHasNfc] = React.useState(null);
   const [enabled, setEnabled] = React.useState(null);
 
-React.useEffect(() => {
-  async function checkNfc() {
-    const supported = await NfcManager.isSupported();
-    if (supported) {
-      await NfcManager.start();
-      setEnabled(await NfcManager.isEnabled());
+  React.useEffect(() => {
+    async function checkNfc() {
+      const supported = await NfcManager.isSupported();
+      if (supported) {
+        await NfcManager.start();
+        setEnabled(await NfcManager.isEnabled());
+      }
+      setHasNfc(supported);
     }
-    setHasNfc(supported);
-  }
-  checkNfc();
-}, []);
+    checkNfc();
+  }, []);
 
-function renderNfcButtons() {
-  if (hasNfc === null) {
-    return null
-  } else if (!hasNfc) {
-    return (
-      <View style={styles.wrapper}>
-        <Text>Your device doesn't support NFC</Text>
-      </View>
-    );
-  } else if (!enabled) {
-    return (
-      <View style={styles.wrapper}>
-        <Text>Your NFC is not enabled</Text>
-        <TouchableOpacity
-        onPress={() => {
-          NfcManager.goToNfcSetting();
-        }}>
-          <Text>GO TO NFC SETTINGS</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        onPress={async () => {
-          setEnabled(await NfcManager.isEnabled());
-        }}>
-          <Text>CHECK AGAIN</Text>
-        </TouchableOpacity>
-      </View>
-    )
+  async function readNdef() {
+    try {
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      const tag = await NfcManager.getTag();
+      navigation.navigate('Tag', { tag })
+    } catch (ex) {
+      // bypass
+    } finally {
+      NfcManager.cancelTechnologyRequest();
+    }
   }
-  return (
-    <View style={styles.bottom}>
-        <Button mode="contained" style={[styles.btn]}
+
+  function renderNfcButtons() {
+    if (hasNfc === null) {
+      return null
+    } else if (!hasNfc) {
+      return (
+        <View style={styles.wrapper}>
+          <Text>Your device doesn't support NFC</Text>
+        </View>
+      );
+    } else if (!enabled) {
+      return (
+        <View style={styles.wrapper}>
+          <Text>Your NFC is not enabled</Text>
+          <TouchableOpacity
+            onPress={() => {
+              NfcManager.goToNfcSetting();
+            }}>
+            <Text>GO TO NFC SETTINGS</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={async () => {
+              setEnabled(await NfcManager.isEnabled());
+            }}>
+            <Text>CHECK AGAIN</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.bottom}>
+        <Button
+          mode="contained"
+          style={[styles.btn]}
           onPress={() => {
-            navigation.navigate('Tag')
+            readNdef();
           }}
-          >
+        >
           TAP
         </Button>
         <Button mode="contained" style={styles.btn}>
           LINK
         </Button>
       </View>
-  )
-}
+    )
+  }
 
   return (
     <View style={styles.wrapper}>
       <View style={styles.wrapper}>
         <Text style={styles.bannerText}>Tap-And-Go</Text>
       </View>
-        {renderNfcButtons()}
+      {renderNfcButtons()}
     </View>
   );
 }
