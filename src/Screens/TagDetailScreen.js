ba8768b8c6b81e7c7b2642e191428a99ec870edb
Louise Naginski <louisindiana@gmail.com>
2023-07-07 11:34:38 -0400

    read and show ndef raw data

--- src/Screens/TagDetailScreen.js
+++ src/Screens/TagDetailScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { Ndef } from 'react-native-nfc-manager';
 
 function TagDetailScreen(props) {
   const {route} = props;
   const {tag} = route.params;
+  let uri = null;
+
+  if (tag.ndefMessage && tag.ndefMessage.length > 0) {
+    const ndefRecord = tag.ndefMessage[0];
+    if(ndefRecord.tnf === Ndef.TNF_WELL_KNOWN) {
+      if(ndefRecord.type.every((b, i) => b === Ndef.RTD_BYTES_URI[i])) {
+        uri = Ndef.uri.decodePayload(ndefRecord.payload);
+      }
+    }
+  }
   return (
     <View style={styles.wrapper}>
       <Text>{JSON.stringify(tag, null, 2)}</Text>
